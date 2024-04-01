7cb8bbad7a4c94ad30c9e68e8666748a553a3b63
Alex <142909895+AlexTCox@users.noreply.github.com>
2024-04-01 18:51:17 -0500

    Updated index with functionality and .use that allows communication

--- Server/index.ts
+++ Server/index.ts
@@ -17,17 +17,12 @@ const pool = new Pool({
     ssl: {rejectUnauthorized: false}
 });
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    if (req.method == "OPTIONS") {
-        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-        return res.status(200).json({});
-    }
-    next();
+app.use(express.json())
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+  next();
 });
 
 process.on('SIGINT', function() {
@@ -54,6 +49,72 @@ app.get('/menu_items', (req, res) => {
             res.status(500).json({ error: error.message });
         });
 });
+
+//---------------Creating Options---------------//
+
+app.post('/new_menu_option', (req,res) => {
+    const {name, price, ingredients} = req.body;
+    console.log(req.body);
+    pool
+        .query("SELECT new_menu_option($1, $2, $3)",[name, parseFloat(price), ingredients])  
+        .then(response => {
+            res.status(200).send(response);
+          })  
+})
+
+app.post('/new_add_on', (req,res) => {
+    const {name, stock, price, minStock} = req.body;
+    console.log(req.body);
+    pool
+        .query("SELECT new_ingredient($1, $2, $3, $4)",[name, Number(stock), parseFloat(price), Number(minStock)])  
+        .then(response => {
+            res.status(200).send(response);
+          })  
+})
+
+app.post('/new_drink', (req,res) => {
+    const {size, price} = req.body;
+    console.log(req.body);
+    pool
+        .query("SELECT new_drink_size($1, $2)",[size, parseFloat(price)])  
+        .then(response => {
+            res.status(200).send(response);
+          })  
+})
+
+//---------------Deleting Options---------------//
+
+app.delete('/delete_drink', (req,res) => {
+    const {size} = req.body;
+    console.log(req.body);
+    pool
+        .query("SELECT delete_drink($1)",[size])  
+        .then(response => {
+            res.status(200).send(response);
+          })  
+})
+
+app.delete('/delete_menu_item', (req,res) => {
+    const {name} = req.body;
+    console.log(req.body);
+    pool
+        .query("SELECT delete_menu_item($1)",[name])  
+        .then(response => {
+            res.status(200).send(response);
+          })  
+})
+
+app.delete('/delete_ingredient', (req,res) => {
+    const {name} = req.body;
+    console.log(req.body);
+    pool
+        .query("SELECT delete_ingredient($1)",[name])  
+        .then(response => {
+            res.status(200).send(response);
+          })  
+})
+
+
     
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
