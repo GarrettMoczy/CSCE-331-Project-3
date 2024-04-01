cc1149a46badc235965afd45481b9f35b5a71223
Garrett Moczygemba <gcmoczy@tamu.edu>
2024-04-01 14:48:51 -0500

    Made pages

---
+++ my-restaurant-app/app/manager/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+import React, { useState, useEffect } from 'react'
+
+
+export default function Manager() {
+
+
+
+    return (
+        <main >
+            <div>
+                    hello
+            </div>
+        </main>
+    );
+}
