3aa28fab20ac53710deac3d2d23631c2840173af
GarrettMoczy <125213735+GarrettMoczy@users.noreply.github.com>
2024-04-01 18:30:46 -0500

    Changed landing pages

--- my-restaurant-app/app/page.tsx
+++ my-restaurant-app/app/page.tsx
@@ -6,55 +6,11 @@ import React, { useState, useEffect } from 'react'
 
 export default function Home() {
 
-    interface MenuItem {
-        name: string;
-        price: string;
-        altTxt: string;
-        thisOnClick: () => void;
-    }
-    
-    function MenuItemClick(){
-        console.log("click")
-    }
-    
-    const [MenuItems, setMenuItems] = useState<MenuItem[]>([]);
-    
-    useEffect(() => {
-        getIngredients();
-    });
-
-    function getIngredients(){
-        fetch(`http://localhost:3000/menu_items`) // Replace with the actual API endpoint URL
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then((data) => {
-                // Process the data received from the API and store it in the state   
-                const menuData: MenuItem[] = data.map((item: any) => ({
-                    name: item.name,
-                    price: item.price,
-                    altTxt: "",
-                    thisOnClick: () => {console.log("click")}
-                }));
-                setMenuItems(menuData);
-        })
-    }
 
     return (
-        <main className="flex flex-col items-center justify-between p-2">
-            <div className="ingredientTabel flex-col justify-center items-center border-rose-700 border-4 h-full w-full overflow-auto rounded-xl">
-                {MenuItems.map((MenuItem, index) => (
-                    <MenuItemComp
-                        key={index}
-                        name={MenuItem.name}
-                        price={MenuItem.price}
-                        thisOnClick={MenuItem.thisOnClick}
-                        altTxt={MenuItem.altTxt}
-                    />
-                ))}
+        <main >
+            <div>
+                Paco Paco mfers
             </div>
         </main>
     );
