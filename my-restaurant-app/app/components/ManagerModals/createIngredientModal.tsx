9fd29e4586681360d35eebe9cdb110eed5d05aff
Alex <142909895+AlexTCox@users.noreply.github.com>
2024-04-23 20:56:29 -0500

    Updated Manager functions
    The updated functions will all have the same button and modal layout. All functions have custom modals that fit their desired needs.

---
+++ my-restaurant-app/app/components/ManagerModals/createIngredientModal.tsx
@@ -0,0 +1,75 @@
+'use client'
+import React, { useState } from "react"
+import "../MenuItemModal/MenuItemModal.css"
+
+interface ModalItemProps {
+    setOpenModal: any;
+    name: string;
+    fun: any;
+}
+
+
+// Prompt the user to delete an item and based on parameters to decide from which
+// setOpenModal - if the modal is open
+// name - Title of modal
+// item - array of which possible items maybe deleted
+// thisOnClick - function used
+function CreateIngredientModal({ setOpenModal, name, fun}: ModalItemProps) {
+    const [ingName, setIngName] = useState("");
+    const [stock, setStock] = useState("");
+    const [price, setPrice] = useState("");
+    const [minStock, setMinStock] = useState("");
+
+    const changeName = (e: any) => {
+        setIngName(e.target.value);
+    }
+    const changeStock = (e: any) => {
+        setPrice(e.target.value);
+    } 
+    const changePrice = (e: any) => {
+        setPrice(e.target.value);
+    } 
+    const changeMinStock = (e: any) => {
+        setPrice(e.target.value);
+    } 
+
+    return (
+    <div className="modalBackground">
+        <div className="'flex flex-col relative flex-wrap border-zinc-700 border-2 bg-zinc-900 rounded-lg overflow-off m-5">
+            <div className="self-end float-right p-2">
+                <button onClick={()=> setOpenModal(false)}> X </button>
+            </div>
+            <div className="flex flex-col">
+                <div className='font-bold text-white'>
+                    {name}
+                </div>
+                <form>
+                    <label>
+                        <br />
+                        Enter Ingredient Name:
+                        <input className="self-center border-2 rounded-md m-1 bg-black w-40 " type="text" onChange={changeName}/>
+                        <br />
+                        <br />
+                        Enter Stock:
+                        <input className="self-center border-2 rounded-md m-1 bg-black w-40" type="text" onChange={changeStock}/>
+                        <br />
+                        <br />
+                        Enter Add-on Price:
+                        <input className="self-center border-2 rounded-md m-1 bg-black w-40" type="text" onChange={changePrice}/>
+                        <br />
+                        <br />
+                        Enter Min-Stock:
+                        <input className="self-center border-2 rounded-md m-1 bg-black w-40" type="text" onChange={changeMinStock}/>
+                    </label>
+                </form>
+            </div>
+            <div>
+                <button onClick={() => {fun(ingName, stock, price, minStock), setOpenModal(false);}} className="self-center border-2 rounded-md bg-black p-1 m-3 w-40"> Submit </button>
+                <button className="self-center border-2 rounded-md bg-black p-1 m-3 w-40" onClick={() => setOpenModal(false)}>Cancel</button>
+            </div>
+        </div>
+    </div>
+    );
+} 
+
+export default CreateIngredientModal;
