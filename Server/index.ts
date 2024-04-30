ef94bebb6637ddf2ce1535f6e975a58c0901560d
Lsv2 <91164127+Lsv2@users.noreply.github.com>
2024-04-30 18:15:23 -0500

    Update index.ts

--- Server/index.ts
+++ Server/index.ts
@@ -23,12 +23,6 @@ const pool = new Pool({
 
 app.use(express.json())
 app.use(requestIp.mw());
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS,SET');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
-    next();
-});
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
