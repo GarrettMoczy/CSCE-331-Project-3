c85d9e822be9ccf92f2d30171442d89f871d91c8
Lsv2 <91164127+Lsv2@users.noreply.github.com>
2024-04-30 18:54:51 -0500

    Update index.ts

--- Server/index.ts
+++ Server/index.ts
@@ -33,6 +33,7 @@ app.use((req, res, next) => {
         res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
         return res.status(200).json({});
     }
+    next();
 });
 
 process.on('SIGINT', function () {
