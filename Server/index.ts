7146c31a3fd49048527148136f3a7fbc30ccea72
Lsv2 <91164127+Lsv2@users.noreply.github.com>
2024-04-30 18:20:23 -0500

    Update index.ts

--- Server/index.ts
+++ Server/index.ts
@@ -24,15 +24,16 @@ const pool = new Pool({
 app.use(express.json())
 app.use(requestIp.mw());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-if (req.method == "OPTIONS") {
-  res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-  return res.status(200).json({});
-}
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header(
+            "Access-Control-Allow-Headers",
+            "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+        );
+    if (req.method == "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+        return res.status(200).json({});
+    }
+});
 
 process.on('SIGINT', function () {
     pool.end();
